4ea7c6f402d23fd91db0e304225abcc8c24a94d2
Michael Towson <MTowson@gmail.com>
2021-11-22 15:43:06 -0800

    add temporary content to ContentGradientCentered

--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -1,10 +1,11 @@
 import classes from './HomePage.module.css'
+import ContentGradientCentered from '../components/Layout/ContentGradientCentered'; 
 
 const HomePage: React.FC = () => {
 
     return (
     <div className = {classes.view__container}>
-        <h1>Homepage content goes here</h1>
+        <ContentGradientCentered/>
     </div>
     )
 
