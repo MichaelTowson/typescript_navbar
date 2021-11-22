548338e37f102d493bfc566811d0d33c6bd750a6
Michael Towson <MTowson@gmail.com>
2021-11-22 15:43:09 -0800

    add temporary content to ContentGradientCentered

--- src/components/Layout/ContentGradientCentered.tsx
+++ src/components/Layout/ContentGradientCentered.tsx
@@ -3,7 +3,8 @@ import classes from "./ContentGradientCentered.module.css"
 const ContentGradientCentered: React.FC = () => {
   return(
     <div className={classes.container}>
-      <p>Content</p>
+        <h1>Deliver digital experiences that people love.</h1>
+        <h2>Co.tribute is the proven platform to rapidly build digital experiences for financial wellness, storytelling and membership growth initiatives.</h2>
     </div>
   )
 }
