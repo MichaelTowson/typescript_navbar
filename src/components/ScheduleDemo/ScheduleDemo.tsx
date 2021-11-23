9d697d8a26d409de6f9319cf5a79d9396143d06d
Michael Towson <MTowson@gmail.com>
2021-11-22 19:50:35 -0800

    add ScheduleDemo pop-up styling

--- src/components/ScheduleDemo/ScheduleDemo.tsx
+++ src/components/ScheduleDemo/ScheduleDemo.tsx
@@ -8,20 +8,40 @@ interface Props {
 const ScheduleDemo: React.FC<Props> = (props) => {
 
 let scheduleDemoVisibile = props.scheduleDemoVisibility
+
 const toggleVisible: () => void = props.toggleVisible
 
   return (
-    <button onClick={toggleVisible}>
+    <>
+      
+      {/* If scheduleDemo's state is set to visible, then... */}
       {scheduleDemoVisibile ?
-        <div className={classes.container}> 
+        <div>
+
+          {/* <Button> loads a blue background around the form, allowing the user to close the form by clicking. */}
+          <button 
+            onClick={toggleVisible}
+            className={classes.container}
+          />
+          
+          {/* POP-UP WINDOW/FORM */}
           <div className={classes.popUp__window}>
-            <h1>Let's schedule a demo</h1> 
-            <h2>Fill out the details below nad we'll schedule a demo for you with the right person.</h2>
-            <button onClick={toggleVisible}>Close Window</button>
+            <button 
+              onClick={toggleVisible}
+              className={classes.popUp__window_exitButton}
+            >
+              X
+            </button>
+            <h3 className={classes.popUp__window_header}>Let's schedule a demo.</h3> 
+            <h4 className={classes.popUp__window_subtext}>Fill out the details below and we'll schedule a demo for you with the right person.</h4>
           </div>
+
+    
         </div>
-        : ""}
-    </button>
+        : ""
+      }
+    
+    </>
   )
 }
 
